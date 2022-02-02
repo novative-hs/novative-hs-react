59c2f1feec0d90aac8be4d18e453c378a6cda6fb
Sadia <sadia.pma@gmail.com>
2022-02-02 14:29:44 +0500

    Token Dynamic

--- Admin-Redux/src/helpers/django-token-access/auth-token-header.js
+++ Admin-Redux/src/helpers/django-token-access/auth-token-header.js
@@ -1,8 +1,6 @@
 export default function authHeader() {
   const obj = JSON.parse(localStorage.getItem("authUser"));
 
-  console.log("Object: ", obj);
-
   if (obj && obj.token) {
     return obj.token;
   } else {
