3e1e6bdc46eaffe13b880fe823c291f8bf4173d1
labhazir <labhazir@gmail.com>
2022-12-15 13:39:51 +0500

    terms change

--- src/components/HorizontalLayout/Footer.js
+++ src/components/HorizontalLayout/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
-import Pdf from 'pdf/terms.pdf';
+import Pdf from 'pdf/LabHazir - Terms & Conditions.pdf';
 
 const Footer = () => {
   
@@ -31,9 +31,12 @@ const Footer = () => {
                   <span>Terms</span>
                 </Link>
               </div> */}
-               <div className = "App">
-               <a href = {Pdf} rel="noopener noreferrer">Terms</a>
-               </div>
+              <div className = "App">
+               <a href = {Pdf} target="_blank"rel="noopener noreferrer">Terms</a>
+              </div>
+               {/* <div className = "App">
+               <a href = {Pdf} rel="noopener">Terms</a>
+               </div> */}
             </Col>
             <Col md={1}>
               <div className="text-sm-end d-none d-sm-block">
