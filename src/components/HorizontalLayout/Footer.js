bcbeb197b0b6dde20b93c96c85a2195ca09067ae
labhazir <labhazir@gmail.com>
2023-06-24 17:44:06 +0500

    set futter in app and web screen view

--- src/components/HorizontalLayout/Footer.js
+++ src/components/HorizontalLayout/Footer.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
-import Pdf from 'pdf/LabHazir - Terms & Conditions.pdf';
 
 const Footer = () => {
-  
+  const isLargeScreen = window.innerWidth > 768;
+  const isSmallScreen = window.innerWidth >= 576 && window.innerWidth <= 768;
+  const isextraSmallScreen = window.innerWidth < 576;
+  const largfixfooter = {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "#f8f9fa",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "center",
+  };
+  const islargfixfooter = {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "#f8f9fa",
+    fontSize: "6.5px",
+    display: "flex",
+    justifyContent: "center",
+  };
+
+  console.log("isLargeScreen:", isLargeScreen);
+  console.log("isSmallScreen:", isSmallScreen);
+
   return (
-    <React.Fragment>
-      <footer className="footer">
-        <Container fluid={true}>
+    <footer className="footer">
+      <Container fluid={true}>
+        {isLargeScreen && (
           <Row>
             <Col md={4}>{new Date().getFullYear()} © Lab Hazir (Private) Limited</Col>
             <Col md={3}>
@@ -26,48 +49,131 @@ const Footer = () => {
               </div>
             </Col>
             <Col md={2}>
-              {/* <div className="text-sm-end d-none d-sm-block">
+              <div className="text-sm-end d-none d-sm-block">
                 <Link to="/terms-conditions">
                   <span>Terms</span>
                 </Link>
-              </div> */}
-              <div className = "App">
-               <a href = {Pdf} target="_blank"rel="noopener noreferrer">Terms</a>
               </div>
-               {/* <div className = "App">
-               <a href = {Pdf} rel="noopener">Terms</a>
-               </div> */}
             </Col>
             <Col md={1}>
               <div className="text-sm-end d-none d-sm-block">
-              <a href="https://www.linkedin.com/company/labhazir/" target="_blank"rel="noopener noreferrer">
-                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />{" "}
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100084984286550" target="_blank" rel="noopener noreferrer">
-                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />{" "}
-              </a>
+                <a
+                  href="https://www.linkedin.com/company/labhazir/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />{" "}
+                </a>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100084984286550"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />{" "}
+                </a>
               </div>
             </Col>
-
-            {/* <Col md={3}>
-              <div className="text-sm-end d-none d-sm-block">
-                Welcome to Lab Hazir Family!
-              
-              </div>
-            </Col> */}
-            {/* <Col>
-            <div className="social-container">
-              <h3>Social Follow</h3>
-              <a href="https://www.youtube.com/c/jamesqquick">
-                <i className="bx bxl-youtube bx-tada align-middle me-2 font-size-30" />{" "}
+          </Row>
+        )}
+{isSmallScreen && (
+  <Row style={largfixfooter} className="footer">
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-start">
+              {new Date().getFullYear()} © Lab Hazir (Private) Limited
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/disclaimer">
+                <span>Disclaimer</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/about-us">
+                <span>About Us</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/terms-conditions">
+                <span>Terms</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <a
+                href="https://www.linkedin.com/company/labhazir/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />
+              </a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100084984286550"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />
               </a>
             </div>
-            </Col> */}
-          </Row>
-
-        </Container>
-      </footer>
-    </React.Fragment>
+          </Col>
+        </Row>
+)}
+{isextraSmallScreen && (
+  <Row style={islargfixfooter} className="footer">
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-start">
+              {new Date().getFullYear()} © Lab Hazir (Private) Limited
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/disclaimer">
+                <span>Disclaimer</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/about-us">
+                <span>About Us</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/terms-conditions">
+                <span>Terms</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <a
+                href="https://www.linkedin.com/company/labhazir/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />
+              </a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100084984286550"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+)}
+        
+      </Container>
+    </footer>
   );
 };
 
--- src/components/VerticalLayout/Footer.js
+++ src/components/VerticalLayout/Footer.js
@@ -3,10 +3,35 @@ import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 
 const Footer = () => {
+  const isLargeScreen = window.innerWidth > 768;
+  const isSmallScreen = window.innerWidth >= 576 && window.innerWidth <= 768;
+  const isextraSmallScreen = window.innerWidth < 576;
+  const largfixfooter = {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "#f8f9fa",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "center",
+  };
+  const islargfixfooter = {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "#f8f9fa",
+    fontSize: "6.5px",
+    display: "flex",
+    justifyContent: "center",
+  };
+
+  console.log("isLargeScreen:", isLargeScreen);
+  console.log("isSmallScreen:", isSmallScreen);
+
   return (
-    <React.Fragment>
-      <footer className="footer">
-        <Container fluid={true}>
+    <footer className="footer">
+      <Container fluid={true}>
+        {isLargeScreen && (
           <Row>
             <Col md={4}>{new Date().getFullYear()} © Lab Hazir (Private) Limited</Col>
             <Col md={3}>
@@ -32,34 +57,123 @@ const Footer = () => {
             </Col>
             <Col md={1}>
               <div className="text-sm-end d-none d-sm-block">
-              <a href="https://www.linkedin.com/company/labhazir/" target="_blank"rel="noopener noreferrer">
-                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />{" "}
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100084984286550" target="_blank" rel="noopener noreferrer">
-                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />{" "}
-              </a>
+                <a
+                  href="https://www.linkedin.com/company/labhazir/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />{" "}
+                </a>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100084984286550"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />{" "}
+                </a>
               </div>
             </Col>
-
-            {/* <Col md={3}>
-              <div className="text-sm-end d-none d-sm-block">
-                Welcome to Lab Hazir Family!
-              
-              </div>
-            </Col> */}
-            {/* <Col>
-            <div className="social-container">
-              <h3>Social Follow</h3>
-              <a href="https://www.youtube.com/c/jamesqquick">
-                <i className="bx bxl-youtube bx-tada align-middle me-2 font-size-30" />{" "}
+          </Row>
+        )}
+{isSmallScreen && (
+  <Row style={largfixfooter} className="footer">
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-start">
+              {new Date().getFullYear()} © Lab Hazir (Private) Limited
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/disclaimer">
+                <span>Disclaimer</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/about-us">
+                <span>About Us</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/terms-conditions">
+                <span>Terms</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <a
+                href="https://www.linkedin.com/company/labhazir/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />
+              </a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100084984286550"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />
               </a>
             </div>
-            </Col> */}
-          </Row>
-
-        </Container>
-      </footer>
-    </React.Fragment>
+          </Col>
+        </Row>
+)}
+{isextraSmallScreen && (
+  <Row style={islargfixfooter} className="footer">
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-start">
+              {new Date().getFullYear()} © Lab Hazir (Private) Limited
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/disclaimer">
+                <span>Disclaimer</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/about-us">
+                <span>About Us</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <Link to="/terms-conditions">
+                <span>Terms</span>
+              </Link>
+            </div>
+          </Col>
+          <Col xs="auto" sm="auto" md="auto" lg="auto">
+            <div className="text-sm-end">
+              <a
+                href="https://www.linkedin.com/company/labhazir/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-linkedin bx-tada align-middle me-2 font-size-40" />
+              </a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100084984286550"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bx bxl-facebook bx-tada align-middle me-2 font-size-40" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+)}
+        
+      </Container>
+    </footer>
   );
 };
 
