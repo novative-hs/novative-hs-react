8461b5a7724ebc9be0c68624f1c8575e966f6c8a
labhazir <labhazir@gmail.com>
2023-08-08 12:22:24 +0500

    cart changes

--- src/pages/LabMarket/NearbyLabs/TestsOfferedbyLab.js
+++ src/pages/LabMarket/NearbyLabs/TestsOfferedbyLab.js
@@ -181,7 +181,7 @@ class TestsOffered extends Component {
         success: this.props.success,
         error: this.props.error
       });
-      console.log("Setting state with end of cart", this.props.success, this.props.error);
+      console.log("Setting state with end of cart", this.state.success, this.state.error);
     }, 100);
   };
   activateParentDropdown = item => {
