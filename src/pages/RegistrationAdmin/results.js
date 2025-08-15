b51e19bef10811a7050ce7396217955f7404daf6
novative-hs <novative.hs@gmail.com>
2025-08-15 11:33:07 +0500

    pagination

--- src/pages/RegistrationAdmin/results.js
+++ src/pages/RegistrationAdmin/results.js
@@ -1085,7 +1085,7 @@ class Results extends Component {
     console.log("Round Status:", this.props.round_status);
 
     const pageOptions = {
-      sizePerPage: 10,
+      sizePerPage: 50,
       totalSize: combinedData.length > 0 ? combinedData.length : " ",
       custom: true,
     };
